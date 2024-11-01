5b49b9ce813bbdce3968083c53e604f3c1b4d3d4
SeongukCho <tjddnr377@naver.com>
2024-11-01 15:04:32 +0900

    MSA Notice Service

---
+++ NoticeService/src/main/java/kopo/poly/config/OpenFeignConfig.java
@@ -0,0 +1,21 @@
+package kopo.poly.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenFeignConfig {
+
+    Logger.Level feignLoggerLevel() {
+
+        /**
+         * Open Feign 을 통해 전송 및 전달받는 모든 과정에 대해 로그 찍기 설정
+         *
+         * NONE : 로깅하지 않음 (기본값)
+         * BASIC : 요청 메소드와 URI 와 응답 상태와 실행시간 로깅함
+         * HEADERS : 요청과 응닫ㅂ 헤더와 함께 기본 정보들을 남김
+         * FULL : 요청과 응답에 대한 헤더와 바디, 메타 데이터를 남김
+         */
+        return Logger.Level.FULL;
+    }
+}
